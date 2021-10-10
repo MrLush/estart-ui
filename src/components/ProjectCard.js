c978b5a32536f1b5c9228a629ecc56f1afcb976d
Anastasiia Moskvina <Anastasiia_Moskvina@epam.com>
2021-10-10 15:07:35 +0300

    add real pictures to project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,19 +1,22 @@
 import { Link } from 'react-router-dom';
 import styles from './ProjectCard.module.scss';
 
-function ProjectCard(props) {
+function ProjectCard({project}) {
+
+  const { image, id, name, stage } = project;
 
   const randomImg = Math.floor(Math.random() * 10);
-  
+  const imgSrc = image && image.length ? image : `https://raw.githubusercontent.com/MrLush/estart-ui/main/src/img/${randomImg}.jpg`;
+
   return (
     <div className={styles.card}>
       <div className={`${styles.card__side} ${styles.card__side__front}`}>
-        <img src={`https://raw.githubusercontent.com/MrLush/estart-ui/main/src/img/${randomImg}.jpg`} alt="project" className={styles.img}/>
-        <h2 className={styles.header}>{props.project.name}</h2>
-        <p className={styles.stage}>{props.project.stage}</p>
+        <img src={imgSrc} alt="project" className={styles.img}/>
+        <h2 className={styles.header}>{name}</h2>
+        <p className={styles.stage}>{stage}</p>
       </div>
       <div className={`${styles.card__side} ${styles.card__side__back}`}>
-        <Link className={styles.link} to={`/current-project/${props.project.id}`}>Get more information</Link>
+        <Link className={styles.link} to={`/current-project/${id}`}>Get more information</Link>
       </div>
     </div>
   )
