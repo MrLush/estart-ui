bb0ee53eff7faa38b97d3fe1ca330ac264156940
Anastasiia Moskvina <Anastasiia_Moskvina@epam.com>
2021-10-10 15:14:25 +0300

    refactor styles

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,19 +4,20 @@ import styles from './ProjectCard.module.scss';
 function ProjectCard({project}) {
 
   const { image, id, name, stage } = project;
+  const { card, card__side, card__side__front, img, header, stage: stageStyle, link, card__side__back } = styles;
 
   const randomImg = Math.floor(Math.random() * 10);
   const imgSrc = image && image.length ? image : `https://raw.githubusercontent.com/MrLush/estart-ui/main/src/img/${randomImg}.jpg`;
 
   return (
-    <div className={styles.card}>
-      <div className={`${styles.card__side} ${styles.card__side__front}`}>
-        <img src={imgSrc} alt="project" className={styles.img}/>
-        <h2 className={styles.header}>{name}</h2>
-        <p className={styles.stage}>{stage}</p>
+    <div className={card}>
+      <div className={`${card__side} ${card__side__front}`}>
+        <img src={imgSrc} alt="project" className={img}/>
+        <h2 className={header}>{name}</h2>
+        <p className={stageStyle}>{stage}</p>
       </div>
-      <div className={`${styles.card__side} ${styles.card__side__back}`}>
-        <Link className={styles.link} to={`/current-project/${id}`}>Get more information</Link>
+      <div className={`${card__side} ${card__side__back}`}>
+        <Link className={link} to={`/current-project/${id}`}>Get more information</Link>
       </div>
     </div>
   )
